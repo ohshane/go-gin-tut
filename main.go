88a355ac1e5829cde72ec7d5f5d6765d847fc1ba
Shane Oh <ohshane71@gmail.com>
2020-12-28 23:45:36 +0900

    refactoring routes with routes.go

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
+
+var router *gin.Engine
 
 func main() {
 	router := gin.Default()
@@ -12,17 +12,6 @@ func main() {
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(
-			http.StatusOK,
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-	})
 
 	router.Run()
 }
