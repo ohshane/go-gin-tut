a48a0e14735897b9dfb8373196078d0861ff9f15
Shane Oh <ohshane71@gmail.com>
2020-12-29 21:54:37 +0900

    refactoring: detatching  routs.go

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var router *gin.Engine
+var router *gin.Engine = gin.Default()
 
 func main() {
-	router := gin.Default()
+	// router := gin.Default()
 
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
