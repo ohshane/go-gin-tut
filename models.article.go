61d4c59e08077904c380574536c8edc596177355
Shane Oh <ohshane71@gmail.com>
2020-12-30 18:45:22 +0900

    added more articles

--- models.article.go
+++ models.article.go
@@ -12,6 +12,8 @@ type article struct {
 var articleList = []article{
 	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
 	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+	article{ID: 3, Title: "Article 3", Content: "Article 3 body"},
+	article{ID: 4, Title: "Article 4", Content: "Article 4 body"},
 }
 
 // Return a list of all the articles
